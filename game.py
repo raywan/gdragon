72202f967db9bdd68b0a2844f4e2c3e72f883139
Raymond Wan <taiyang.rw@gmail.com>
2013-05-16 21:57:53 -0400

    State manager added. Cleanest shit ever.

--- game.py
+++ game.py
@@ -2,36 +2,82 @@ import sys
 import pygame
 from pygame.locals import *
 
+#To be interfaced from
 class EmptyState(object):
+    def __init__(self, parent):
+        pass
     def update(self):
         pass
+    def render(self, screen):
+        pass
+    def on_enter(self, args):
+        pass
+    def on_exit(self):
+        pass
+
+class MainMenu(object):
+    def __init__(self, parent):
+        self.game = parent
+    def event(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                if event.key == K_p:
+                    self.game.game_state.change("play", None)
+    def update(self):
+        pygame.display.flip()
     def render(self):
+        self.game.screen.blit(self.main_surf, (0,0))
+    def on_enter(self, args):
+        self.main_surf = pygame.Surface((300,200), 0, 32)
+    def on_exit(self):
         pass
-    def on_enter(self):
-        pass
+
+class Play(object):
+    def __init__(self, parent):
+        self.game = parent
+    def event(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                if event.key == K_m:
+                    self.game.game_state.change("main", None)
+    def update(self):
+        pygame.display.flip()
+    def render(self):
+        self.game.screen.blit(self.play_surf, (0,0))
+    def on_enter(self, args):
+        self.play_surf = pygame.Surface((300,200), 0, 32)
+        self.play_surf.fill((250,250,250))
     def on_exit(self):
         pass
 
 class StateMachine(object):
     #list of states
-    #current_state = empty_state
+    empty_state = EmptyState(None)
+    current_state = empty_state
+    states = {}
+    def event(self):
+        self.current_state.event()
     def update(self):
-        #current_state.update()
-        pass
+        self.current_state.update()
     def render(self):
-        #current_state.render()
-        pass
+        self.current_state.render()
     def change(self, state_name, other_args):
-        #current_state.on_exit()
-        #current_state = list_of_states[state_name]
-        #current_state.on_enter(other_args)
-        pass
+        self.current_state.on_exit()
+        self.current_state = self.states[state_name]
+        self.current_state.on_enter(other_args)
     def add(self, state_name, state_class):
         #list_of_states[state_name] = state_class
-        pass
+        self.states[state_name] = state_class
 
 class StateStack(object):
     #list of states
+    states = {}
     def update(self):
         #update top stack
         #top.Update()
@@ -53,33 +99,38 @@ class StateStack(object):
 class Game(object):
     def __init__(self):
         self.running = True
+        self.SCREEN_WIDTH = 300
+        self.SCREEN_HEIGHT = 200
     def on_init(self):
-        pygame.init()
         #set screen and load stuff
+        pygame.init()
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT), 0, 32)
+        self.clock = pygame.time.Clock()
+
+        self.game_state = StateMachine()
+        self.game_state.add("main", MainMenu(self))
+        self.game_state.add("play", Play(self))
+        self.game_state.change("main", None)
+        
     def on_event(self):
-        pass
+        self.game_state.event()
     def on_update(self):
-        #game_state.update()
-        #game_state.render()
-        pass
+        self.game_state.update()
     def on_render(self):
-        pass
+        self.game_state.render()
+
     def on_exec(self):
         self.on_init()
 
         while self.running:
-            clock.tick(60)
-            # self.on_event()
-            # self.on_loop()
-            # self.on_render()
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-        # self.on_cleanup()
-
+            self.clock.tick(60)
+            self.on_event()
+            self.on_update()
+            self.on_render()
 
 def main():
     game = Game()
     game.on_exec()
+
+if __name__ == "__main__":
+    main()
