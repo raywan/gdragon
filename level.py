0dc0cf788dabaa6ce737bc3b18d4813c6501eaa0
Garikai Gluch <gluchtheclutch@gmail.com>
2013-06-06 11:43:55 -0400

    more sprites

--- level.py
+++ level.py
@@ -101,6 +101,94 @@ class MainMap(object):
                     surf.blit(self.cliff_grass_left_center, (32*(tile_column - 1), 32*(tile_row - 1)))
                 elif col == '6':
                     surf.blit(self.cliff_grass_right_center, (32*(tile_column - 1), 32*(tile_row - 1)))
+                elif col == '7':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("BerryBush", x, y)
+                elif col == '8':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("Bush", x, y)
+                elif col == '9':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("BigTreeOne", x, y)
+                elif col == 'h':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("BigTreeTwo", x, y)
+                elif col == 'i':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("BigTreeThree", x, y)
+                elif col == 'j':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("BigTreeFour", x, y)
+                elif col == 'k':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("BigTreeFive", x, y)
+                elif col == 'l':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("BigTreeSix", x, y)
+                elif col == 'm':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("BigTreeSeven", x, y)
+                elif col == 'n':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("BigTreeEight", x, y)
+                elif col == 'o':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("BigTreeNine", x, y)
+                elif col == 'p':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("BigTreeTen", x, y)
+                elif col == 'q':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("BigTreeEleven", x, y)
+                elif col == 'r':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("BigTreeTwelve", x, y)
+                elif col == 's':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("BigTreeThirteen", x, y)
+                elif col == 't':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("BigTreeFourteen", x, y)
+                elif col == 'u':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("BigTreeFifteen", x, y)
+                elif col == 'v':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("BigTreeSixteen", x, y)
+                elif col == 'w':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("LargeLogsOne", x, y)
+                elif col == 'x':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("LargeLogsTwo", x, y)
+                elif col == 'y':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("LargeLogsThree", x, y)
+                elif col == 'z':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("LargeLogsFour", x, y)
                 elif col == 'a':
                     x = 32*(tile_column - 1)
                     y = 32*(tile_row - 1)
@@ -113,6 +201,46 @@ class MainMap(object):
                     x = 32*(tile_column - 1)
                     y = 32*(tile_row - 1)
                     self.render_solids("SmallCliffCenter", x, y)
+                elif col == '!':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("CliffSideLargeLeftTop", x, y)
+                elif col == '@':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("CliffSideLargeMiddleTop", x, y)
+                elif col == '#':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("CliffSideLargeRightTop", x, y)
+                elif col == '$':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("CliffSideLargeLeftCenter", x, y)
+                elif col == '%':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("CliffSideLargeMiddleCenter", x, y)
+                elif col == '^':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("CliffSideLargeRightCenter", x, y)
+                elif col == '&':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("CliffSideLargeRightCenter", x, y)
+                elif col == '*':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("CliffSideLargeLeftBottom", x, y)
+                elif col == '*':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("CliffSideLargeMiddleBottom", x, y)
+                elif col == '*':
+                    x = 32*(tile_column - 1)
+                    y = 32*(tile_row - 1)
+                    self.render_solids("CliffSideLargeRightBottom", x, y)
                 elif col == 'd':
                     x = 32*(tile_column - 1)
                     y = 32*(tile_row - 1)
@@ -169,6 +297,130 @@ class MainMap(object):
             self.top = Top(x,y)
             self.all_sprite.add(self.top)
             self.solid_list.add(self.top)
+        elif solid == "BerryBush":
+            self.berrybush = BerryBush(x,y)
+            self.all_sprites.add(self.berrybush)
+            self.solid_list.add(self.berrybush)
+        elif solid == "Bush":
+            self.bush = Bush(x,y)
+            self.all_sprites.add(self.bush)
+            self.solid_list.add(self.bush)
+        elif solid == "BigTreeOne":
+            self.big_tree_one = BigTreeOne(x,y)
+            self.all_sprites.add(self.big_tree_one)
+            self.solid_list.add(self.big_tree_one)
+        elif solid == "BigTreeTwo":
+            self.big_tree_two = BigTreeTwo(x,y)
+            self.all_sprites.add(self.big_tree_two)
+            self.solid_list.add(self.big_tree_two)
+        elif solid == "BigTreeThree":
+            self.big_tree_three = BigTreeThree(x,y)
+            self.all_sprites.add(self.big_tree_three)
+            self.solid_list.add(self.big_tree_three)
+        elif solid == "BigTreeFour":
+            self.big_tree_four = BigTreeFour(x,y)
+            self.all_sprites.add(self.big_tree_four)
+            self.solid_list.add(self.big_tree_four)
+        elif solid == "BigTreeFive":
+            self.big_tree_five = BigTreeFive(x,y)
+            self.all_sprites.add(self.big_tree_five)
+            self.solid_list.add(self.big_tree_five)
+        elif solid == "BigTreeSix":
+            self.big_tree_six = BigTreeSix(x,y)
+            self.all_sprites.add(self.big_tree_six)
+            self.solid_list.add(self.big_tree_six)
+        elif solid == "BigTreeSeven":
+            self.big_tree_seven = BigTreeSeven(x,y)
+            self.all_sprites.add(self.big_tree_seven)
+            self.solid_list.add(self.big_tree_seven)
+        elif solid == "BigTreeEight":
+            self.big_tree_eight = BigTreeEight(x,y)
+            self.all_sprites.add(self.big_tree_eight)
+            self.solid_list.add(self.big_tree_eight)
+        elif solid == "BigTreeNine":
+            self.big_tree_nine = BigTreeNine(x,y)
+            self.all_sprites.add(self.big_tree_nine)
+            self.solid_list.add(self.big_tree_nine)
+        elif solid == "BigTreeTen":
+            self.big_tree_ten = BigTreeTen(x,y)
+            self.all_sprites.add(self.big_tree_ten)
+            self.solid_list.add(self.big_tree_ten)
+        elif solid == "BigTreeEleven":
+            self.big_tree_eleven = BigTreeEleven(x,y)
+            self.all_sprites.add(self.big_tree_eleven)
+            self.solid_list.add(self.big_tree_eleven)
+        elif solid == "BigTreeTwelve":
+            self.big_tree_twelve = BigTreeTwelve(x,y)
+            self.all_sprites.add(self.big_tree_twelve)
+            self.solid_list.add(self.big_tree_twelve)
+        elif solid == "BigTreeThirteen":
+            self.big_tree_thirteen = BigTreeThirteen(x,y)
+            self.all_sprites.add(self.big_tree_thirteen)
+            self.solid_list.add(self.big_tree_thirteen)
+        elif solid == "BigTreeFourteen":
+            self.big_tree_fourteen = BigTreeFourteen(x,y)
+            self.all_sprites.add(self.big_tree_fourteen)
+            self.solid_list.add(self.big_tree_fourteen)
+        elif solid == "BigTreeFifteen":
+            self.big_tree_fifteen = BigTreeFifteen(x,y)
+            self.all_sprites.add(self.big_tree_fifteen)
+            self.solid_list.add(self.big_tree_fifteen)
+        elif solid == "BigTreeSixten":
+            self.big_tree_sixteen = BigTreeSixteen(x,y)
+            self.all_sprites.add(self.big_tree_sixteen)
+            self.solid_list.add(self.big_tree_sixteen)
+        elif solid == "LargeLogsOne":
+            self.large_logs_one = LargeLogsOne(x,y)
+            self.all_sprites.add(self.large_logs_one)
+            self.solid_list.add(self.large_logs_one)
+        elif solid == "LargeLogsTwo":
+            self.large_logs_two = LargeLogsTwo(x,y)
+            self.all_sprites.add(self.large_logs_two)
+            self.solid_list.add(self.large_logs_two)
+        elif solid == "LargeLogsThree":
+            self.large_logs_three = LargeLogsThree(x,y)
+            self.all_sprites.add(self.large_logs_three)
+            self.solid_list.add(self.large_logs_three)
+        elif solid == "LargeLogsFour":
+            self.large_logs_four = LargeLogsFour(x,y)
+            self.all_sprites.add(self.large_logs_four)
+            self.solid_list.add(self.large_logs_four)
+        elif solid == "CliffSideLargeLeftTop":
+            self.cliff_side_large_left_top = CliffSideLargeLeftTop(x,y)
+            self.all_sprites.add(self.cliff_side_large_left_top)
+            self.solid_list.add(self.cliff_side_large_left_top)
+        elif solid == "CliffSideLargeMiddleTop":
+            self.cliff_side_large_middle_top = CliffSideLargeMiddleTop(x,y)
+            self.all_sprites.add(self.cliff_side_large_middle_top)
+            self.solid_list.add(self.cliff_side_large_middle_top)
+        elif solid == "CliffSideLargeRightTop":
+            self.cliff_side_large_right_top = CliffSideLargeRightTop(x,y)
+            self.all_sprites.add(self.cliff_side_large_right_top)
+            self.solid_list.add(self.cliff_side_large_right_top)
+        elif solid == "CliffSideLargeLeftCenter":
+            self.cliff_side_large_left_center = CliffSideLargeLeftCenter(x,y)
+            self.all_sprites.add(self.cliff_side_large_left_center)
+            self.solid_list.add(self.cliff_side_large_left_center)
+        elif solid == "CliffSideLargeMiddleCenter":
+            self.cliff_side_large_middle_center = CliffSideLargeMiddleCenter(x,y)
+            self.all_sprites.add(self.cliff_side_large_middle_center)
+            self.solid_list.add(self.cliff_side_large_middle_center)
+        elif solid == "CliffSideLargeRightCenter":
+            self.cliff_side_large_right_center = CliffSideLargeRightCenter(x,y)
+            self.all_sprites.add(self.cliff_side_large_right_center)
+            self.solid_list.add(self.cliff_side_large_right_center)
+        elif solid == "CliffSideLargeLeftBottom":
+            self.cliff_side_large_left_bottom = CliffSideLargeLeftBottom(x,y)
+            self.all_sprites.add(self.cliff_side_large_left_bottom)
+            self.solid_list.add(self.cliff_side_large_left_bottom)
+        elif solid == "CliffSideLargeMiddleBottom":
+            self.cliff_side_large_middle_bottom = CliffSideLargeMiddleBottom(x,y)
+            self.all_sprites.add(self.cliff_side_large_middle_bottom)
+            self.solid_list.add(self.cliff_side_large_middle_bottom)
+        elif solid == "CliffSideLargeRightBottom":
+            self.cliff_side_large_right_bottom = CliffSideLargeRightBottom(x,y)
+            self.all_sprites.add(self.cliff_side_large_right_bottom)
+            self.solid_list.add(self.cliff_side_large_right_bottom)
         else:
             print "None" 
     def render_enterable(self, enterable, x, y):
@@ -285,23 +537,27 @@ class CaveMap(object):
     def render_solids(self, solid, x, y):
         if solid == "rock":
             self.rock = RockTile(x,y)
+            self.all_sprites.add(self.rock)
             self.solid_list.add(self.rock)
         elif solid == "CaveWalls":
             self.cave_walls = CaveWalls(x,y)
+            self.all_sprites.add(self.cave_walls)
             self.solid_list.add(self.cave_walls)
         elif solid == "LeftBorder":
             self.left = Left(x,y)
-            
+            self.all_sprites.add(self.left)
             self.solid_list.add(self.left)
         elif solid == "RightBorder":
             self.right = Right(x,y)
+            self.all_sprites.add(self.right)
             self.solid_list.add(self.right)
         elif solid == "BottomBorder":
             self.bottom = Bottom(x,y)
-        
+            self.all_sprites.add(self.bottom)
             self.solid_list.add(self.bottom)
         elif solid == "TopBorder":
             self.top = Top(x,y)
+            self.all_sprites.add(self.top)
             self.solid_list.add(self.top)
         else:
             print "None" 
